2323f956e5f656893b628d14d2736e9644914be5
Aysen23 <borisov_aisen@mail.ru>
2021-11-02 11:31:28 +0900

    add space_game

--- game_space/ufo.py
+++ game_space/ufo.py
@@ -7,7 +7,7 @@ class Ufo(pygame.sprite.Sprite):
         """инициализируем и задаём начальную позицию"""
         super(Ufo, self).__init__()
         self.screen = screen
-        self.image = pygame.image.load('images/ufo.png')
+        self.image = pygame.image.load('images/pixil-frame-0 (1).png')
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
